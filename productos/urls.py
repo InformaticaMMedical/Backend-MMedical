c96eb983413a3ec8c880b0a72d23b2d27537afb8
Brigitte <brial.murillo@gmail.com>
2025-10-30 16:50:12 -0300

    Commit 10 - Modificacion de urls

--- productos/urls.py
+++ productos/urls.py
@@ -1,16 +1,18 @@
 from django.urls import path
-from productos.views.AtributoValorView import ValorAtributoProductoDetailAPIView, ValorAtributoProductoListCreateAPIView
-from productos.views.AtributoView import AtributoDetailAPIView, AtributoListCreateAPIView
-from productos.views.CategoriaView import CategoriaDetailAPIView, CategoriaListCreateAPIView
-from productos.views.ProveedorView import ProveedorDetailAPIView, ProveedorListCreateAPIView
-from productos.views.ProductoView import ProductoDetailAPIView, ProductoListCreateAPIView
 
-# Fabricantes
-from productos.views.FabricanteView import FabricanteDetailAPIView, FabricanteListCreateAPIView
-from productos.views.ModeloFabricanteView import ModeloFabricanteDetailAPIView, ModeloFabricanteListCreateAPIView
-from productos.views.ProductoCompatibilidadView import ProductoCompatibilidadDetailAPIView, ProductoCompatibilidadListCreateAPIView
+# Vistas principales
+from productos.views.AtributoView import AtributoListCreateAPIView, AtributoDetailAPIView
+from productos.views.AtributoValorView import ValorAtributoProductoListCreateAPIView, ValorAtributoProductoDetailAPIView
+from productos.views.CategoriaView import CategoriaListCreateAPIView, CategoriaDetailAPIView
+from productos.views.ProveedorView import ProveedorListCreateAPIView, ProveedorDetailAPIView
+from productos.views.ProductoView import ProductoListCreateAPIView, ProductoDetailAPIView
+from productos.views.ProductoCompatibilidadView import ProductoCompatibilidadListCreateAPIView, ProductoCompatibilidadDetailAPIView
 
-from django.urls import path
+# Fabricantes y modelos
+from productos.views.FabricanteView import FabricanteListCreateAPIView, FabricanteDetailAPIView
+from productos.views.ModeloFabricanteView import ModeloFabricanteListCreateAPIView, ModeloFabricanteDetailAPIView
+
+# Imágenes
 from productos.views.ImagenesProductoView import (
     ImagenesProductoCollectionView,
     ImagenProductoDetailView,
@@ -18,45 +20,38 @@ from productos.views.ImagenesProductoView import (
 
 urlpatterns = [
     # Categorías
-    path('categorias/', CategoriaListCreateAPIView.as_view(), name='categorias'),
-    path('categorias/<int:pk>/', CategoriaDetailAPIView.as_view(), name='categoria-detalle'),
+    path('categorias/', CategoriaListCreateAPIView.as_view(), name='categorias-list'),
+    path('categorias/<int:pk>/', CategoriaDetailAPIView.as_view(), name='categorias-detail'),
 
     # Proveedores
-    path('proveedores/', ProveedorListCreateAPIView.as_view(), name='proveedores'),
-    path('proveedores/<int:pk>/', ProveedorDetailAPIView.as_view(), name='proveedor-detalle'),
+    path('proveedores/', ProveedorListCreateAPIView.as_view(), name='proveedores-list'),
+    path('proveedores/<int:pk>/', ProveedorDetailAPIView.as_view(), name='proveedores-detail'),
 
     # Atributos
-    path('atributos/', AtributoListCreateAPIView.as_view(), name='atributos'),
-    path('atributos/<int:pk>/', AtributoDetailAPIView.as_view(), name='atributo-detalle'),
+    path('atributos/', AtributoListCreateAPIView.as_view(), name='atributos-list'),
+    path('atributos/<int:pk>/', AtributoDetailAPIView.as_view(), name='atributos-detail'),
 
     # Valores de Atributo
-    path('atributos/valores/', ValorAtributoProductoListCreateAPIView.as_view(), name='atributo-valores'),
-    path('atributos/valores/<int:pk>/', ValorAtributoProductoDetailAPIView.as_view(), name='atributo-valor-detalle'),
+    path('atributos/valores/', ValorAtributoProductoListCreateAPIView.as_view(), name='atributos-valores-list'),
+    path('atributos/valores/<int:pk>/', ValorAtributoProductoDetailAPIView.as_view(), name='atributos-valores-detail'),
 
     # Productos
-    path('productos/', ProductoListCreateAPIView.as_view(), name='productos'),
-    path('productos/<int:pk>/', ProductoDetailAPIView.as_view(), name='producto-detalle'),
+    path('productos/', ProductoListCreateAPIView.as_view(), name='productos-list'),
+    path('productos/<int:pk>/', ProductoDetailAPIView.as_view(), name='productos-detail'),
 
     # Fabricantes
-    path('fabricantes/', FabricanteListCreateAPIView.as_view(), name='fabricantes'),
-    path('fabricantes/<int:pk>/', FabricanteDetailAPIView.as_view(), name='fabricante-detalle'),
+    path('fabricantes/', FabricanteListCreateAPIView.as_view(), name='fabricantes-list'),
+    path('fabricantes/<int:pk>/', FabricanteDetailAPIView.as_view(), name='fabricantes-detail'),
 
     # Modelos de Fabricante
-    path('modelos/', ModeloFabricanteListCreateAPIView.as_view(), name='modelos'),
-    path('modelos/<int:pk>/', ModeloFabricanteDetailAPIView.as_view(), name='modelo-detalle'),
+    path('modelos-fabricante/', ModeloFabricanteListCreateAPIView.as_view(), name='modelos-fabricante-list'),
+    path('modelos-fabricante/<int:pk>/', ModeloFabricanteDetailAPIView.as_view(), name='modelos-fabricante-detail'),
 
     # Compatibilidad de Productos
-    path('compatibilidades/', ProductoCompatibilidadListCreateAPIView.as_view(), name='compatibilidades'),
-    path('compatibilidades/<int:pk>/', ProductoCompatibilidadDetailAPIView.as_view(), name='compatibilidad-detalle'),
+    path('compatibilidades/', ProductoCompatibilidadListCreateAPIView.as_view(), name='compatibilidades-list'),
+    path('compatibilidades/<int:pk>/', ProductoCompatibilidadDetailAPIView.as_view(), name='compatibilidades-detail'),
 
-    # Modelos de fabricantes
-    path("modelos/", ModeloFabricanteListCreateAPIView.as_view(), name="modelos-list"),
-    path("modelos/<int:pk>/", ModeloFabricanteDetailAPIView.as_view(), name="modelos-detail"),
-
-
-    # Imagenes
-    path("productos/<int:producto_id>/imagenes/", ImagenesProductoCollectionView.as_view(), name="producto_imagenes"),
-    path("productos/imagenes/<int:imagen_id>/", ImagenProductoDetailView.as_view(), name="producto_imagen_detalle"),
-
-
+    # Imágenes de Producto
+    path('productos/<int:producto_id>/imagenes/', ImagenesProductoCollectionView.as_view(), name='imagenes-producto-list'),
+    path('imagenes/<int:imagen_id>/', ImagenProductoDetailView.as_view(), name='imagenes-producto-detail'),
 ]
